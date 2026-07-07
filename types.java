30ff55c33f63bb14af339f6987dd539158caf7e4
coder-utshab <ghoshutshob470@gmail.com>
2026-07-07 21:17:00 +0530

    up

---
+++ types.java
@@ -0,0 +1,18 @@
+public class types {
+    public static void main(String[] args) {
+        int myNum = 5;
+        System.out.println(myNum);
+
+
+    float myFloatNum = 2.56f;
+    System.out.println(myFloatNum);
+
+    char myLetter = 'D';
+    System.out.println(myLetter);
+
+    boolean myBool = true;
+    System.out.println(myBool);
+    }
+}
+    
+
