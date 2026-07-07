cfc0c396f3e6361e244aec5923dc9d47916bade0
coder-utshab <ghoshutshob470@gmail.com>
2026-07-07 20:55:38 +0530

    up

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Student data
+        String studentName = "Utshab Ghosh";
+        int studentId = 15;
+        int studentAge = 23;
+        float studentFee = 72.25f;
+        char studentGrade = 'B';
+
+        // Print variables
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);
+    }
+}
