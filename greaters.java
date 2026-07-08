901d3c82b7220bac565fa40319508d625769fc17
coder-utshab <ghoshutshob470@gmail.com>
2026-07-08 09:23:06 +0530

    up

---
+++ greaters.java
@@ -0,0 +1,7 @@
+public class greaters {
+    public static void main(String[] args) {
+        //greater or less than
+        System.out.println(10 > 5);
+        System.out.println(3 < 2);
+    }
+}
---
+++ logicals.java
@@ -0,0 +1,8 @@
+public class logicals {
+    public static void main(String[] args) {
+        //and opearator
+        int x = 4;
+        System.out.println(x < 5 && x < 10);
+    }
+    
+}
