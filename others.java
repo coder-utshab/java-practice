73eeaef1bf093bafdbe7e8a9b2f50c1a55740021
coder-utshab <ghoshutshob470@gmail.com>
2026-07-08 09:16:20 +0530

    up

---
+++ others.java
@@ -0,0 +1,20 @@
+public class others {
+    public static void main(String[] args) {
+        
+//other opearators
+int x = 5;
+int y = 10;
+int z = 15;
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+
+
+x *= 3;
+y /= 2;
+z %= 2;
+System.out.println(x);  
+System.out.println(y);
+System.out.println(z);
+    }
+}
