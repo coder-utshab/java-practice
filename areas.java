fdc93a362ea489d09a5d5f51ddd0a5e864b52e42
coder-utshab <ghoshutshob470@gmail.com>
2026-07-09 23:42:39 +0530

    up

---
+++ areas.java
@@ -0,0 +1,16 @@
+public class areas {
+    public static void main(String[] args){
+        //create integer variable
+        int length = 4;
+        int width = 6;
+        int area;
+
+        //calculate the area of rectangle
+        area = length * width;
+
+        //print variables
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: "+ width);
+        System.out.println("Area of the Rectangle is: " + area);  
+    }
+}
