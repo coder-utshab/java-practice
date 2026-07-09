834078828aa6c61e6c1efd8223eded52879ad386
coder-utshab <ghoshutshob470@gmail.com>
2026-07-09 23:56:35 +0530

    up

---
+++ remember.java
@@ -0,0 +1,10 @@
+public class remember {
+    public static void main(String[] args){
+
+        String name = "Utshab";
+        int age = 24;
+
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
