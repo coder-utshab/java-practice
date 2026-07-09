09dcb634f089f77cba0d584a71c1ea2b5e90378c
coder-utshab <ghoshutshob470@gmail.com>
2026-07-09 22:51:01 +0530

    up

---
+++ minuites.java
@@ -0,0 +1,11 @@
+public class minuites {
+    public static void main(String[] args){
+        int minuitesPerHour = 60;
+        
+        // ok but not so easy to understand what m actually is
+
+        int m = 80;
+        System.out.println(minuitesPerHour);
+        System.out.println(m);
+    }
+}
