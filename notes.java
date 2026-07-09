d40704f70fbab62dcbf48fedd3f3dc2690e5208c
coder-utshab <ghoshutshob470@gmail.com>
2026-07-09 22:02:21 +0530

    up

---
+++ notes.java
@@ -0,0 +1,22 @@
+// public class notes {
+    
+// }
+
+
+
+
+// 
+
+//  This is a single-line comment//
+
+/*
+ This is a multi-line comment 
+*/
+
+
+
+// Comments in Java are written with special characters. Insert the missing parts:
+// This is a single-line comment
+/*
+ This is a multi-line comment 
+*/
---
+++ variables.java
@@ -0,0 +1,27 @@
+public class variables {
+    public static void main(String[] args) {
+        //string
+        String name = "Utshab";
+
+        //int
+        int level = 5;
+
+        //float
+        float health = 98.5f;
+
+        // char
+        char status = 'A';
+
+        //boolean
+        boolean isGameOver = false;
+
+        //printting them out
+        System.out.println("Player: " + name);
+        System.out.println("Level: " + level);
+        System.out.println("Health: " + health);
+        System.out.println("Staatus: " + status);
+        System.out.println("Game Over?" + isGameOver);
+    }
+}
+
+
