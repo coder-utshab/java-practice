88a9c3ad1f3ef49420e798b7e5fd03ea3d889bee
coder-utshab <ghoshutshob470@gmail.com>
2026-07-10 16:17:42 +0530

    up

---
+++ sums.java
@@ -0,0 +1,9 @@
+public class sums {
+    public static void main(String[] args) {
+          int itemPrice = 50;
+    int shippingCost = 10;
+    int sum = itemPrice + shippingCost;
+
+    System.out.println(sum);
+    }
+}
