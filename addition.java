12cde6204b9c4f5bb22689789a6750333a24931a
coder-utshab <ghoshutshob470@gmail.com>
2026-07-11 23:59:42 +0530

    up

---
+++ addition.java
@@ -0,0 +1,8 @@
+public class addition {
+    public static void main(String[] args) {
+    int x = 10;
+    int y = 20;
+    int z = x + y;
+    System.out.println(z);
+  }
+}
--- concat.java
+++ concat.java
@@ -8,3 +8,9 @@ public class concat {
 }
 
 
+//concat() কী করে?
+// concat() একটি String Method। এটি একটি String-এর শেষে আরেকটি String যোগ করে।
+
+// কখন concat() ব্যবহার করব?
+// concat() → শুধুমাত্র String-এর সাথে String যোগ করার জন্য।
+// + → String-এর সাথে String, int, double, boolean সবকিছু যোগ করা যায়।
