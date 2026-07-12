cb522948f86d9f52d44dca36b509f586576c7e4b
coder-utshab <ghoshutshob470@gmail.com>
2026-07-12 19:49:20 +0530

    sdf

---
+++ booleans.java
@@ -0,0 +1,42 @@
+// public class booleans {
+//     public static void main(String[] args){
+// int x= 10;
+//     int y = 20;
+
+//     boolean isGreater = x > y;
+//     System.out.println(isGreater);
+//     }
+    
+// }
+
+
+
+
+
+
+// public class booleans {
+//   public static void main(String[] args) {
+//     int myAge = 25;
+//     int votingAge = 18;
+//     System.out.println(myAge >= votingAge); // returns true (25 year olds are allowed to vote!) 
+//   }
+// }
+
+
+
+
+
+
+public class booleans {
+  public static void main(String[] args) {
+    int myAge = 25;
+    int votingAge = 18;
+
+    if (myAge >= votingAge) {
+        System.out.println("Oldenough to vote");
+    }
+    else{
+        System.out.println("Not old enogh to vote");
+    }
+;  }
+}
