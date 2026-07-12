e61d0656a461346140867de7b9a329c808cb4012
coder-utshab <ghoshutshob470@gmail.com>
2026-07-12 21:41:18 +0530

    zd

--- nopearetor.java
+++ nopearetor.java
@@ -1,10 +1,28 @@
-public class nopearetor {
-     public static void main(String[] args) {
-    int a = 33;
-    int b = 200;
+// public class nopearetor {
+//      public static void main(String[] args) {
+//     int a = 33;
+//     int b = 200;
 
-    if (!(a > b)) {
-      System.out.println("a is NOT greater than b");
+//     if (!(a > b)) {
+//       System.out.println("a is NOT greater than b");
+//     }
+//   }
+// }
+
+
+
+
+
+public class nopearetor {
+    public static void main(String[] args) {
+        boolean isLoggedIn = true;
+        boolean isAdmin = false;
+        int securityLevel = 3;
+
+        if (isLoggedIn && (isAdmin || securityLevel <= 2)) {
+            System.out.println("Access granted");
+        } else {
+            System.out.println("Access denied");
+        }
     }
-  }
 }
