db583a56970b523e85ace91a6cb30697defa40b8
coder-utshab <ghoshutshob470@gmail.com>
2026-07-12 21:44:26 +0530

    dzcx

--- nopearetor.java
+++ nopearetor.java
@@ -13,16 +13,34 @@
 
 
 
+// public class nopearetor {
+//     public static void main(String[] args) {
+//         boolean isLoggedIn = true;
+//         boolean isAdmin = false;
+//         int securityLevel = 3;
+
+//         if (isLoggedIn && (isAdmin || securityLevel <= 2)) {
+//             System.out.println("Access granted");
+//         } else {
+//             System.out.println("Access denied");
+//         }
+//     }
+// }
+
+
+
 public class nopearetor {
     public static void main(String[] args) {
-        boolean isLoggedIn = true;
-        boolean isAdmin = false;
-        int securityLevel = 3;
 
-        if (isLoggedIn && (isAdmin || securityLevel <= 2)) {
-            System.out.println("Access granted");
+        int myNum = 10; // Is this a positive or negative number?
+
+        if (myNum > 0) {
+            System.out.println("The value is a positive number.");
+        } else if (myNum < 0) {
+            System.out.println("The value is a negative number.");
         } else {
-            System.out.println("Access denied");
+            System.out.println("The value is 0.");
         }
+
     }
 }
