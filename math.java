59e81d2c619f5444ece38f79f574235e8da93bce
coder-utshab <ghoshutshob470@gmail.com>
2026-07-12 19:11:20 +0530

    czxc

--- math.java
+++ math.java
@@ -5,8 +5,25 @@
 // }
 
 
+// 
+
+
+
 public class math {
-    public static void main(String []args) {
-        System.out.println(Math.sqrt(64));
-    }
+  public static void main(String[] args) {
+    System.out.println(Math.sqrt(64));  
+  }
 }
+
+
+
+
+
+
+
+
+// public class  math {
+//   public static void main(String[] args) {
+//     System.out.println(Math.pow(2,8));
+//   }
+// }
