b5c718d6e1534fc66f828f8bc38a5e44d7e6b0f5
coder-utshab <ghoshutshob470@gmail.com>
2026-07-12 21:08:06 +0530

    awds

--- condition.java
+++ condition.java
@@ -1,15 +1,14 @@
-public class condition{
+public class condition {
     public static void main(String[] args) {
         int weather = 2;
-
 
         if (weather == 1) {
             System.out.println("Bring on umbrella");
 
-        }else if (weather == 2) {
-           System.out.println("wear sunglass"); 
+        } else if (weather == 2) {
+            System.out.println("wear sunglass");
 
-        }else {
+        } else {
             System.out.println("just go outside normally");
         }
     }
