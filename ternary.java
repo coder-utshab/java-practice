f35facc748aaa6dcd0747999bf506c69e397b979
coder-utshab <ghoshutshob470@gmail.com>
2026-07-12 21:18:49 +0530

    wsad

---
+++ ternary.java
@@ -0,0 +1,11 @@
+public class ternary {
+    public static void main(String[] args) {
+
+        int score = 95;
+
+        String grade = (score >= 33) ? "Pass" : "Fail";
+
+        System.out.println(grade);
+
+    }
+}
