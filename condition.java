3d7c345e6f231bfdb9c0b046671ea00fe6c0ce03
coder-utshab <ghoshutshob470@gmail.com>
2026-07-12 21:03:35 +0530

    zc

---
+++ condition.java
@@ -0,0 +1,16 @@
+public class condition{
+    public static void main(String[] args) {
+        int weather = 2;
+
+
+        if (weather == 1) {
+            System.out.println("Bring on umbrella");
+
+        }else if (weather == 2) {
+           System.out.println("wear sunglass"); 
+
+        }else {
+            System.out.println("just go outside normally");
+        }
+    }
+}
