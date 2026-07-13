9a4158ae5a5fc03c7f9289a9d1a5344bc07f84a9
coder-utshab <ghoshutshob470@gmail.com>
2026-07-13 22:49:09 +0530

    vcvvvvfd

--- does.java
+++ does.java
@@ -1,12 +1,18 @@
 public class does {
     public static void main(String[] args) {
 
-        int i = 0;
+        // int i = 0;
 
-        do {
-            System.out.println(i);
-            i++;
-        } while (i < 3);
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i < 3);
+        int i = 5;
+
+do {
+    System.out.println(i);
+    i++;
+} while (i < 5);
 
     }
 }
