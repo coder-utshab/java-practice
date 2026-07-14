117178979da654ca97b9ca818f107f6c7a201610
coder-utshab <ghoshutshob470@gmail.com>
2026-07-14 20:00:01 +0530

    sdazx

---
+++ continues.java
@@ -0,0 +1,17 @@
+
+//continue ar kag holo jamon if ar man 2 daoa aca ata run korba nahhh
+ //shudhu oi man tay cholba nahh tarpor thaka abar cholba
+
+public class continues{
+  public static void main(String[] args) {
+    for (int i = 0; i < 6; i++) {
+      if (i == 3) {
+        continue;
+      }
+      if (i == 4) {
+        break;
+      }
+      System.out.println(i);
+    }
+  }
+}
