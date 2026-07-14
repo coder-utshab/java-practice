c46c243e801d0762e1d3d24346af2cce74863619
coder-utshab <ghoshutshob470@gmail.com>
2026-07-14 21:30:26 +0530

    hgfgb

--- breaks.java
+++ breaks.java
@@ -8,20 +8,35 @@
 // thaktacha 9 akhon 9  ajonno count hoba nahh karon tar aga break use hoyacha
 
 
-public class breaks {
-    public class Main {
-  public static void main(String[] args) {
-    int[] numbers = {3, -1, 7, 0, 9};
+// public class breaks {
+//     public class Main {
+//   public static void main(String[] args) {
+//     int[] numbers = {3, -1, 7, 0, 9};
 
-    for (int n : numbers) {
-      if (n < 0) {
-        continue; // skip negative numbers
-      }
-      if (n == 0) {
-        break; // stop loop when zero is found
-      }
-      System.out.println(n);
-    }
+//     for (int n : numbers) {
+//       if (n < 0) {
+//         continue; // skip negative numbers
+//       }
+//       if (n == 0) {
+//         break; // stop loop when zero is found
+//       }
+//       System.out.println(n);
+//     }
+//   }
+// }
+// }
+
+
+
+
+public class breaks{
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+  if (i == 3) {
+    ;
   }
+  System.out.println(i);
+}
+
 }
 }
