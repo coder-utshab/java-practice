a7ab1f2ca3aa6ccd09fe3efac4d1873a09c3640b
coder-utshab <ghoshutshob470@gmail.com>
2026-07-14 18:38:41 +0530

    sdcxds

---
+++ switchs.java
@@ -0,0 +1,28 @@
+public class switchs {
+   public static void main(String[] args){
+    int day = 4;
+    switch (day){
+
+        case 1:
+            System.out.println("Monaday");
+            break;
+
+            case 2:
+            System.out.println("Tuesday");
+            break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+                case 4:
+                    System.out.println("Thuersday");
+                    break;
+
+                    case 5:
+                        System.out.println("Friday");
+                        break;
+
+      }
+   } 
+}
