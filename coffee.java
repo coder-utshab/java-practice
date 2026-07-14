170a7718256a60507b56c8e361e790d8d775b50a
coder-utshab <ghoshutshob470@gmail.com>
2026-07-14 18:57:20 +0530

    df

---
+++ coffee.java
@@ -0,0 +1,21 @@
+public class coffee {
+    public static void main(String[] args){
+
+        int choice = 2;
+
+
+        switch (choice){
+            case 1:
+                System.out.println("my name is a utshab");
+                break;
+                
+                case 2:
+                    System.out.println("i studey in parul university");
+                    break;
+
+                    default:
+                        System.out.println("i am from bangladesh");
+                        break;
+        }
+    }
+}
