aed30e420da0fbf2d15287bb71e7b9c2ddd9c1a4
coder-utshab <ghoshutshob470@gmail.com>
2026-07-17 22:20:11 +0530

    gry

--- myaddition.java
+++ myaddition.java
@@ -1,9 +1,23 @@
-public class myaddition {
-    static int myMethod(int x){
-        return 5 + x;
+// public class myaddition {
+//     static int myMethod(int x){
+//         return 5 + x;
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(myMethod(3));
+//     }
+// }
+
+
+
+
+public class myaddition{
+    static int myMethod(int x , int y){
+        return x + y;
     }
 
-    public static void main(String[] args) {
-        System.out.println(myMethod(3));
+    public static void main(String[] args){
+        int z = myMethod(5,3);
+        System.out.println(z);
     }
 }
