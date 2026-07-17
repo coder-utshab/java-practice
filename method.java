aa0c295b9334c1957d20690e18cefb3d6bc75596
coder-utshab <ghoshutshob470@gmail.com>
2026-07-17 21:32:56 +0530

    nhkh

---
+++ method.java
@@ -0,0 +1,23 @@
+// public class methods {
+//     static void welcomeMessage(){
+//         System.out.println("Wealcome to java");
+//     }
+//     public static void main(String[] args){
+//         welcomeMessage();
+//     }
+// }
+
+
+
+public class method {
+
+    static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
+    }
+
+    public static void main(String[] args) {
+        myMethod("Utshab");
+        myMethod("Sajib");
+        myMethod("Dipto");
+    }
+}
--- methods.java
+++
@@ -1,8 +0,0 @@
-public class methods {
-    static void welcomeMessage(){
-        System.out.println("Wealcome to java");
-    }
-    public static void main(String[] args){
-        welcomeMessage();
-    }
-}
