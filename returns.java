bdb62d34c942b3abab83db5029f8abab929ea2ac
coder-utshab <ghoshutshob470@gmail.com>
2026-07-17 22:34:53 +0530

    up

---
+++ returns.java
@@ -0,0 +1,8 @@
+public class returns {
+    static void wealcomeMessage(String name){
+        System.out.println("Wealcome to java," + name);
+    }
+    public static void main(String[] args){
+        wealcomeMessage("Utshab");
+    }
+}
