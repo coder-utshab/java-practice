efaac63de66caada9cd0d43bd5100b19e95cfbb0
coder-utshab <ghoshutshob470@gmail.com>
2026-07-19 23:56:03 +0530

    up

--- overloadings.java
+++ overloadings.java
@@ -1,10 +1,18 @@
 public class overloadings {
+
     static int plusMethods(int x, int y) {
         return x + y;
-
     }
-    static double plusMethodDouble(double x, double y){
+
+    static double plusMethodDouble(double x, double y) {
         return x + y;
     }
-    
+
+    public static void main(String[] args) {
+        int myNum1 = plusMethods(8, 5);
+        double myNum2 = plusMethodDouble(4.3, 6.26);
+
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+    }
 }
