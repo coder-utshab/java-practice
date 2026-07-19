ba596e79931796a0eb34e2fe0588a7b492a74eaf
coder-utshab <ghoshutshob470@gmail.com>
2026-07-19 23:50:10 +0530

    up

---
+++ overloadings.java
@@ -0,0 +1,10 @@
+public class overloadings {
+    static int plusMethods(int x, int y) {
+        return x + y;
+
+    }
+    static double plusMethodDouble(double x, double y){
+        return x + y;
+    }
+    
+}
