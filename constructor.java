10cf295205613025d51388161c31d99d1b5aafd1
coder-utshab <ghoshutshob470@gmail.com>
2026-07-21 09:07:56 +0530

    up

--- constructor.java
+++ constructor.java
@@ -1,8 +1,15 @@
+import java.util.objects;
+class Student{
+
+    int a=5;
+}
 public class constructor {
     int a;
     public static void main (String[]args){
+        Student s = new Student();
+        Student s1 = new Student();
 
-        System.out.println("Utshab");
+        System.out.println(s.equals(s1));
 
     }
 }
