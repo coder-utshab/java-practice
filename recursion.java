b8312b330b8337ba32e07d9341c889cb3078eb64
coder-utshab <ghoshutshob470@gmail.com>
2026-08-02 10:49:42 +0530

    up

--- recursion.java
+++ recursion.java
@@ -33,15 +33,34 @@
 
 
 
-public class recursion {
-    static int sum (int k){
-        if (k > 0)
-            return k + sum (k - 1);
+// public class recursion {
+//     static int sum (int k){
+//         if (k > 0)
+//             return k + sum (k - 1);
 
-        else
-            return 0;
-    }
-    public static void main (String [] args){
-        System.out.println(sum(5));
-    }
-}
+//         else
+//             return 0;
+//     }
+//     public static void main (String [] args){
+//         System.out.println(sum(5));
+//     }
+// }
+
+// output 15
+
+
+
+
+
+
+// public class recursion {
+//     static int print (int x){
+//         if(x == 0)
+//             return 0;
+
+//         return x + print(x - 1);
+//     }
+//     public static void main(String[] args){
+//         System.out.println(print(4));
+//     }
+// }
