913d07a8bf96b15edc3c5bf2e9d63c80f67dddc4
coder-utshab <ghoshutshob470@gmail.com>
2026-08-03 22:33:31 +0530

    up

---
+++ oop.java
@@ -0,0 +1,9 @@
+public class oop {
+    int x = 5;
+    int y = 10;
+
+    public static void main(String[] args){
+        oop myObj = new oop();
+        System.out.println(myObj.x + " " + myObj.y);
+    }
+}
