fcf25525d3c8c3af85d9cb9c27b30292f26f6ea7
coder-utshab <ghoshutshob470@gmail.com>
2026-08-03 22:58:04 +0530

    up

--- oop.java
+++ oop.java
@@ -11,15 +11,30 @@
 
 
 
+// public class oop {
+//     int x = 15;
+//     int y = 20;
+
+//     public static void main (String[] args){
+//         oop myObj1 = new oop();
+//         oop myObj2 = new oop();
+
+//         System.out.println(myObj1.x);
+//         System.out.println(myObj2.y);
+//         // System.out.println(myObj2.y);
+//     }
+// }
+
+
+
+
 public class oop {
-    int x = 15;
-    int y = 20;
+  int x = 5;
 
-    public static void main (String[] args){
-        oop myObj1 = new oop();
-        oop myObj2 = new oop();
+  public static void main (String[] args){
 
-        System.out.println(myObj1.x);
-        System.out.println(myObj2.y);
-    }
+    oop myObj = new oop();
+    myObj . x = 40;
+    System.out.println(myObj.x);
+  }
 }
