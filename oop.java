4231a53498ee539276fd3cfc363966be4243d0e0
coder-utshab <ghoshutshob470@gmail.com>
2026-08-03 23:10:06 +0530

    up

--- oop.java
+++ oop.java
@@ -8,9 +8,6 @@
 //     }
 // }
 
-
-
-
 // public class oop {
 //     int x = 15;
 //     int y = 20;
@@ -25,9 +22,6 @@
 //     }
 // }
 
-
-
-
 // public class oop {
 //   int x = 5;
 
@@ -39,18 +33,29 @@
 //   }
 // }
 
+// public class oop{
+// int x = 5;
 
+//   public static void main(String[] args) {
+//     oop myObj1 = new oop();  // Object 1
+//     oop myObj2 = new oop();  // Object 2
+//     myObj2.x = 25;
+//     System.out.println(myObj1.x);  // Outputs 5
+//     System.out.println(myObj2.x);  // Outputs 25
+//    }
+// }
 
+public class oop {
+    String fname = "Utshab";
+    String lname = "sajib";
+    int age = 24;
 
+    public static void main(String[] args) {
 
-public class oop{
-int x = 5;
+        oop myObj = new oop();
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Age:" + myObj.age);
 
-  public static void main(String[] args) {
-    oop myObj1 = new oop();  // Object 1
-    oop myObj2 = new oop();  // Object 2
-    myObj2.x = 25;
-    System.out.println(myObj1.x);  // Outputs 5
-    System.out.println(myObj2.x);  // Outputs 25
-   }
+        
+    }
 }
