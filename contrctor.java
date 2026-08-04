1f46a1b315a05441178afedb3bbea3ff4f2a079e
coder-utshab <ghoshutshob470@gmail.com>
2026-08-05 00:27:05 +0530

    up

---
+++ contrctor.java
@@ -0,0 +1,53 @@
+// import java.util.objects;
+// class Student{
+
+//     int a=5;
+// }
+// public class constructor {
+//     int a;
+//     public static void main (String[]args){
+//         Student s = new Student();
+//         Student s1 = new Student();
+
+//         System.out.println(s.equals(s1));
+
+//     }
+// }
+
+
+
+// class Student {
+
+//     int a = 5;
+// }
+
+// public class constructor {
+
+//     public static void main(String[] args) {
+
+//         Student s1 = new Student();
+//         Student s = new Student();
+
+//         System.out.println(s.equals(s1));
+//     }
+// }
+
+
+
+
+
+
+
+
+public class contrctor{
+    int x;
+
+    public contrctor(int y){
+        x = y;
+    }
+    public static void main(String[] args){
+        contrctor myObj = new contrctor(5);
+        System.out.println(myObj.x);
+
+    }
+}
