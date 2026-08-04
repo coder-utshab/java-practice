db14ac742485b238c2fe1577d706c455ab4ef7c5
coder-utshab <ghoshutshob470@gmail.com>
2026-08-04 08:53:00 +0530

    up

---
+++ challange.java
@@ -0,0 +1,11 @@
+public class challange {
+    String city = "New york city";
+    int population = 8000000;
+
+    public static void main(String[] args){
+        challange myObj = new challange();
+
+        System.out.println(myObj.city);
+        System.out.println(myObj.population);
+    }
+}
