87be2eb0086d1be99fb5c9b14ed36f08faaa12c3
coder-utshab <ghoshutshob470@gmail.com>
2026-08-14 23:30:14 +0530

    up

--- forloop.java
+++ forloop.java
@@ -1,3 +1,10 @@
 public class forloop {
-    
+    public static void main(String[] args){
+        for (int i = 0; i < 3; i++){
+            System.out.println("loop 1:"+i);
+        }
+        for(int i = 0; i < 2; i++){
+            System.out.println("loop 2:" + i);
+        }
+    }
 }
