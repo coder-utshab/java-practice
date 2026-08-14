234c3b09cd2f01d3086c7d038243b35d1edb67b9
coder-utshab <ghoshutshob470@gmail.com>
2026-08-14 21:14:11 +0530

    up

--- parameter.java
+++ parameter.java
@@ -1,12 +1,33 @@
-public class parameter {
+// public class parameter {
     
-  static void myMethod(String fname, int age) {
-    System.out.println(fname + " is " + age);
-  }
+//   static void myMethod(String fname, int age) {
+//     System.out.println(fname + " is " + age);
+//   }
 
-  public static void main(String[] args) {
-    myMethod("Liam", 5);
-    myMethod("Jenny", 8);
-    myMethod("Anja", 31);
-  }
+//   public static void main(String[] args) {
+//     myMethod("Liam", 5);
+//     myMethod("Jenny", 8);
+//     myMethod("Anja", 31);
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+// return values
+
+public class parameter{
+  static int myMethod(int x){
+    return 5+x;
+    }
+
+    public static void main(String[] args){
+      System.out.println(myMethod(4));
+    }
 }
